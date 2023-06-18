269281833dc201ca051475e3f64edd6ee891980f
unknown <irfanadiparestyo27@gmail.com>
2023-06-18 20:12:19 +0700

    commit 2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -27,7 +27,7 @@ Hogwarts is home to a student body of between 300 and 400 students, around fifte
                   href="#"
                   class="px-4 py-2 my-2 text-gray-800 uppercase bg-transparent border-2 border-gray-800 md:mt-16 dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 dark:text-white hover:bg-gray-800 hover:text-white text-md"
                 >
-                  CONNECT WITH ME
+                  CONNECT WITH ME!
                 </a>
               </div>
             </div>
